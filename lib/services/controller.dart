9ad4e2d9fe2d68a656fac1fca1ebaa4258c391d4
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2022-11-11 23:57:54 +0100

    Created GetX controller file

---
+++ lib/services/controller.dart
@@ -0,0 +1,36 @@
+import 'package:country_app/model/country_model.dart';
+import 'package:country_app/services/api_service.dart';
+import 'package:get/get.dart';
+
+class CountryController extends GetxController {
+  final _countryService = ApiServices();
+  List<Country> _countryList = [];
+  List<Country> displayList = [];
+  bool isLoading = true;
+  bool errorOccurred = false;
+
+  Future<void> callApi() async {
+      errorOccurred = false;
+      changeBool(true);
+      try{_countryList = await _countryService.fetchCountries();}catch(e) {errorOccurred = true;}
+      changeBool(false);
+      update();
+  }
+
+  void getCountryList() async {
+    await callApi();
+    displayList = _countryList;
+    update();
+  }
+
+  void searchCountry(value) {
+    displayList = _countryList.where((country) => country.name!.toLowerCase().contains(value.toLowerCase())).toList();
+    update();
+  }
+
+
+  void changeBool(bool newValue) {
+    isLoading = newValue;
+    update();
+  }
+}
