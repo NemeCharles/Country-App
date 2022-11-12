667cce698c62a41bf09974ec1b729c271d73c74b
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2022-11-12 15:33:11 +0100

    Deleted some codes

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,38 +2,6 @@ import 'dart:convert';
 import 'package:country_app/model/country_model.dart';
 import 'package:http/http.dart' as http;
 
-// class ApiServices {
-//   Future<List<Country>> fetchCountries() async {
-//     List<Country> _countryList = [];
-//     final response = await http.get(Uri.parse('https://restcountries.com/v2/all'));
-//     if( response.statusCode == 200) {
-//       List<dynamic> values = <dynamic>[];
-//       values = json.decode(response.body);
-//       if(values.isNotEmpty) {
-//         for(int i = 0; i<values.length; i++) {
-//           if(values[i] != null) {
-//             Map<String, dynamic> map = values[i];
-//             _countryList.add(Country.fromJson(map));
-//           }
-//         }
-//       }
-//       // print(_countryList[0].subRegion);
-//       print(_countryList[0].flag!['png']);
-//       // print(_countryList[8].callingCode);
-//       // print(_countryList[0].region);
-//       // print(_countryList[8].name);
-//       // print(_countryList[8].capital);
-//       // print(_countryList[0].language);
-//       // print(_countryList[0].area);
-//       // print(_countryList[0].timeZone);
-//       // print(_countryList[0].population);
-//       // print(_countryList[0].independent);
-//       // print(_countryList[162].currency.runtimeType);
-//       // print(_countryList.length);
-//       return _countryList;
-//     } else{ throw Exception('Unable to load the stuff');}
-//   }
-// }
 class ApiServices {
   Future<List<Country>> fetchCountries() async {
     List<Country> _countryList = [];
@@ -52,13 +20,14 @@ class ApiServices {
       print(_countryList[0].subregion);
       print(_countryList[0].flag.runtimeType);
       print(_countryList[0].coatOfArm);
-      print(_countryList[162].callingCodeRoot);
+      print(_countryList[162].callingCodeRoot.runtimeType);
       print(_countryList[162].callingCodeSuffix.runtimeType);
       print(_countryList[0].region);
       print(_countryList[8].name);
       print(_countryList[8].capital);
       print(_countryList[0].language!.values.first);
       print(_countryList[0].area);
+      print(_countryList[0].currency);
       print(_countryList[0].timeZone![0]);
       print(_countryList[0].population);
       print(_countryList[0].drivingSide);
