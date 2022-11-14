69975c7a1c6acf035dd9a31f5c75be9bc7f1dea4
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2022-11-14 01:14:11 +0100

    Changed class ana parameter names

--- lib/model/filter_models.dart
+++ lib/model/filter_models.dart
@@ -1,5 +1,14 @@
-class ContinentModel {
-  String? continent;
-  bool isChecked = false;
-  ContinentModel({this.continent, this.isChecked = false});
+import 'package:flutter/material.dart';
+
+class ItemsModel {
+  String? title;
+  Widget? checkBox;
+  ItemsModel({this.checkBox, this.title});
 }
+
+class FilterModel {
+  String? filterText;
+  bool isChecked = false;
+  FilterModel({this.filterText, this.isChecked = false});
+}
+
